1fe92536f972c8aef592d71fdf3807f67fd3e7c0
안찬영 <chan20172@gmail.com>
2023-12-06 18:05:40 +0900

    day06 simple-diary 실습 진행중

---
+++ simplediary/src/App.js
@@ -0,0 +1,69 @@
+import { useRef, useState } from 'react';
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "안찬영",
+//     content: "하이 1",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "홍길동",
+//     content: "하이 2",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "아무개",
+//     content: "하이 3",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   }
+// ]
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id != targetId);
+    console.log(newDiaryList);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) => it.id === targetId ? {...it, content: newContent} : it)
+    );
+  };
+
+  return (
+    <div classNmae="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
+    </div>
+  );
+}
+
+export default App;
