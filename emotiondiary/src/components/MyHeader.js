cd583cd85ea3d0754804234e02dda8f36c655a96
안찬영 <chan20172@gmail.com>
2023-12-08 15:33:03 +0900

    day08 감정일기장 실습 진행

---
+++ emotiondiary/src/components/MyHeader.js
@@ -0,0 +1,11 @@
+const MyHeader = ({headText, leftchild, rightchild}) => {
+    return (
+        <header>
+            <div className="head_btn_left">{leftchild}</div>
+            <div className="head_text">{headText}</div>
+            <div className="head_btn_right">{rightchild}</div>
+        </header>
+    );
+};
+
+export default MyHeader;
